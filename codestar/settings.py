df080c6842f2431dd146bc8d92f742b1d21e3631
carl.dunne <carl.m.dunne@hotmail.com>
2024-04-10 13:27:23 +0100

    Added Neon Database

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)g*p-s5*tth4=a0gq#3nt!45r-5_t$g_84!7tfu&^!f13q_9(m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1",
                 ".herokuapp.com"]
@@ -84,9 +84,8 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #    }
 #}
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get)(postgres://unxpag1nkwj:1DqRuoslkLDa@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/fetch_front_baton_856116)
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
